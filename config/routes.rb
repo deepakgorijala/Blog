bd765d6f7eeb1c68bd574f46598de4941b2c02e7
Deepak Gorijala <deepak.gorijala@hotmail.com>
2016-11-04 17:04:52 -0600

    Submission

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  resources :posts
+  resources :comments
+
+  resources :posts do
+    resources :comments
+  end
+
+  root to: "posts#index"
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
