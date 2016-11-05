3e16087c38191d255f85312af29e5636016428d0
Deepak Gorijala <deepak.gorijala@hotmail.com>
2016-11-04 19:47:50 -0600

    Final Update

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -68,11 +68,7 @@ class CommentsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_comment
-
       @comment = Comment.find(params[:id])
-
-      @comment = @post.comments.find(params[:id])
-
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
